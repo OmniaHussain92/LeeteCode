5462c1ad4986f3e8619c3a8136e1fc33310ec425
Omnia Hussain <31737706+OmniaHussain92@users.noreply.github.com>
2021-03-26 22:24:02 +0200

    Adding 13. Roman to Integer

--- LeeteCode/12.IntegertoRoman.cs
+++ LeeteCode/12.IntegertoRoman.cs
@@ -12,6 +12,7 @@ namespace LeeteCode
             string[] roman = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
             int[] nums = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
 
+           
             StringBuilder finaleRoman = new StringBuilder();
 
             for (int i = 0; i < 13; i++)
---
+++ LeeteCode/13.RomantoInteger.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace LeeteCode
+{
+    public class RomantoInteger
+    {
+        public int RomanToInt(string s)
+        {
+
+            // Rules from problem discription
+            Dictionary<string, int> map = new Dictionary<string, int>()
+            {
+                {"M", 1000 },
+                {"CM", 900 },
+                {"D", 500 },
+                {"CD", 400 },
+                {"C", 100 },
+                {"XC", 90 },
+                {"L", 50 },
+                {"XL", 40},
+                {"X", 10 },
+                {"IX", 9 },
+                {"V", 5 },
+                {"IV", 4 },
+                {"I", 1 }
+           };
+
+            int number = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                var curr = s[i].ToString();
+                if (i < s.Length - 1 && (curr == "I" || curr == "X" || curr == "C"))
+                {
+                    var sub = curr + s[i + 1];
+                    if (map.ContainsKey(sub))
+                    {
+                        number += map[sub];
+                        i++;
+                        continue;
+                    }
+
+                }
+
+                number += map[curr];
+
+            }
+
+            return number;
+        }
+    }
+}
