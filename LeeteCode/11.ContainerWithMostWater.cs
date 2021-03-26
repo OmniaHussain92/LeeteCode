cc1d44b0e925cf8f871db0c8842440b5ea3f2b4c
Omnia Hussain <31737706+OmniaHussain92@users.noreply.github.com>
2021-03-26 16:52:42 +0200

    Adding 11.ContainerWithMostWater

---
+++ LeeteCode/11.ContainerWithMostWater.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeeteCode
+{
+    public class ContainerWithMostWater
+    {
+        public int MaxArea(int[] height)
+        {
+            int max = 0;
+            int leftPtr = 0;
+            int rightPtr = height.Length - 1;
+
+            while (leftPtr < rightPtr)
+            {
+                int leftHeight = height[leftPtr];
+                int rightHeight = height[rightPtr];
+
+                if (leftHeight < rightHeight)
+                {
+                    max = Math.Max(leftHeight * (rightPtr - leftPtr), max);
+                    leftPtr++;
+                }
+                else
+                {
+
+                    max = Math.Max(rightHeight * (rightPtr - leftPtr), max);
+                    rightPtr--;
+                }
+            }
+
+            return max;
+        }
+    }
+}
