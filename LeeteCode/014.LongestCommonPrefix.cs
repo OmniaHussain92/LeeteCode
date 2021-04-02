ce9a565cd6b467c1d8825d3a4b60714c97e9282a
Omnia Hussain <31737706+OmniaHussain92@users.noreply.github.com>
2021-04-02 13:56:08 +0200

    Adding 14. Longest Common Prefix

---
+++ LeeteCode/014.LongestCommonPrefix.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeeteCode
+{
+   public class LongestCommonPrefix
+    {
+        public string GetLongestCommonPrefix(string[] strs)
+        {
+            string common = string.Empty;
+            int strsLen = strs.Length;
+
+            if (strsLen < 1)
+                return common;
+
+            char currChar;
+
+            for (int currIndex = 0; currIndex < strs[0].Length; currIndex++)
+            {
+                currChar = strs[0][currIndex];
+
+                for (int i = 1; i < strsLen; i++)
+                {
+                    if (strs[i].Length <= currIndex || strs[i][currIndex] != currChar)
+                    {
+                        return common;
+                    }
+                }
+
+                common += currChar.ToString();
+            }
+
+            return common;
+        }
+    }
+}
