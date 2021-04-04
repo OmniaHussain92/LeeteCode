8237bf9a3e336747154b3b884c9944d2dfcd2ea1
Omnia Hussain <31737706+OmniaHussain92@users.noreply.github.com>
2021-04-04 19:16:46 +0200

    Adding 15. ThreeSum

---
+++ LeeteCode/015.ThreeSum.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeeteCode
+{
+    class _015ThreeSum
+    {
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            int numsLen = nums.Length;
+
+            if (numsLen < 3)
+            {
+                return new List<IList<int>>();
+            }
+
+
+            IList<IList<int>> res = new List<IList<int>>();
+
+            Array.Sort(nums);
+
+            int left = 0; int right = 0; int sum = 0;
+            for (int i = 0; i < numsLen - 2; i++)
+            {
+                if (i > 0 && nums[i] == nums[i - 1])
+                    continue;
+
+                left = i + 1;
+                right = numsLen - 1;
+                while (left < right)
+                {
+                    sum = nums[i] + nums[left] + nums[right];
+
+                    if (sum == 0)
+                    {
+                        res.Add(new List<int>() { nums[i], nums[left], nums[right] });
+                        left++;
+
+                        while (left < right && nums[left] == nums[left - 1]) left++;
+                    }
+                    else if (sum < 0)
+                    {
+                        left++;
+                    }
+                    else
+                    {
+                        right--;
+                    }
+                }
+
+            }
+
+            return res;
+
+        }
+    }
+}
