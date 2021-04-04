73e1ef88d80b912ab1361e4f0aabf3ec8caa86e8
Omnia Hussain <31737706+OmniaHussain92@users.noreply.github.com>
2021-04-04 19:37:58 +0200

    Adding 16. 3Sum Closest

---
+++ LeeteCode/016.3SumClosest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace LeeteCode
+{
+    public class _016_3Sum_Closest
+    {
+        public int ThreeSumClosest(int[] nums, int target)
+        {
+
+            int numsLen = nums.Length;
+
+            if (numsLen < 3)
+            {
+                return 0;
+            }
+
+            Array.Sort(nums);
+            int diff = Int32.MaxValue; int closestSum = 0;
+            int left = 0; int right = 0; int curSum = 0;
+            for (int i = 0; i < numsLen - 2; i++)
+            {
+                if (i > 0 && nums[i] == nums[i - 1])
+                    continue;
+
+                left = i + 1;
+                right = numsLen - 1;
+
+                while (left < right)
+                {
+                    curSum = nums[i] + nums[left] + nums[right];
+                    if (curSum == target)
+                    {
+                        return curSum;
+                    }
+                    if (Math.Abs(curSum - target) < diff)
+                    {
+                        diff = Math.Abs(curSum - target);
+                        closestSum = curSum;
+                    }
+                    if (curSum < target)
+                    {
+                        left++;
+                    }
+                    else
+                    {
+                        right--;
+                    }
+
+                }
+
+            }
+
+            return closestSum;
+
+        }
+    }
+}
